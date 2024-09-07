a00defc8c171289ccd653f8711a90a31632d97c0
Philip osir Otieno <philiposir2@gmail.com>
2024-09-07 20:16:22 +0300

    updates

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,32 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+
+    file_name = str(file_name)
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+    
+    with open(file_name, "w", encoding="utf-8") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+
+    file_name = str(file_name)
+
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+    
+    
+    with open(file_name, "a", encoding="utf-8") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+
+    file_name = str(file_name)
+    
+    if not file_name.endswith(".txt"):
+        file_name += ".txt"
+    
+    with open(file_name, "r", encoding="utf-8") as file:
+        return file.read()
